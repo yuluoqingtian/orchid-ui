ac881e32b608296e0fcf644c1dedd6fe08b729ab
雨洛晴天依旧 <1390589978@qq.com>
2020-12-17 15:59:23 +0800

    前期准备

--- orchid-ui/src/App.tsx
+++ orchid-ui/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
 function App() {
+  const a = 123
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <h1>HELLO WORLD</h1>
+      <h2>HELLO WORLD</h2>
+      <h3>HELLO WORLD</h3>
+      <hr />
+      <code>const a = '123';</code>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
